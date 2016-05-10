cb29ca20615caab4bfa62a3290f20b74cbe13b53
Andrey Novikov <dromgnh@gmail.com>
2016-05-10 23:24:38 +0300

    it works, but I have excess Datagram

--- src/ThreadSender.java
+++ src/ThreadSender.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
 /**
@@ -9,10 +10,11 @@ import java.util.Scanner;
  */
 public class ThreadSender extends Thread {
     public void run() {
-        byte[] msg = new byte[2500];
+        byte[] msg = new byte[65235];
         DatagramSocket ds = null;
         DatagramPacket dp;
         try {
+            //temporary have to type them manually until detecting LAN ips doesn't work
             InetAddress otherAddress = InetAddress.getByName("192.168.1.120");
             InetAddress myAddress = InetAddress.getByName("192.168.1.127");
             ds = new DatagramSocket(6666, myAddress);
@@ -20,20 +22,29 @@ public class ThreadSender extends Thread {
             ds.setReuseAddress(true);
             Scanner sc = new Scanner(System.in);
             while (true) {
-                System.out.println("Input comand to send");
-                String message = sc.nextLine();
-                dp = new DatagramPacket(message.getBytes(), message.length(), otherAddress, 55555);
-                ds.send(dp);
-                System.out.println("sent!");
-                String text = " ";
-                char a = ' ';
-                //because of uncut cbuf in receiver's class I can't detect, when to stop receiving answers
-                for ( ; a != '>'; ) {
-                    dp = new DatagramPacket(msg, msg.length);
-                    ds.receive(dp);
-                    text = new String(msg, 0, dp.getLength());
-                    System.out.println(text);
-                    a = text.charAt(text.length() - 1);
+                try {
+                    System.out.println("Input comand to send");
+                    String message = sc.nextLine();
+                    dp = new DatagramPacket(message.getBytes(), message.length(), otherAddress, 55555);
+                    ds.send(dp);
+                    System.out.println("sent!");
+                    String text;
+                    char a = ' ';
+                    //because of uncut cbuf in receiver's class I can't detect, when to stop receiving answers 45
+                    while (a != '>') {
+                        dp = new DatagramPacket(msg, msg.length);
+                        ds.setSoTimeout(2000);
+                        ds.receive(dp);
+                        text = new String(msg, 0, dp.getLength());
+                        System.out.println(text);
+                        int n = text.length() - 1;
+                        //finding first non-empty character
+                        while((a = text.charAt(n)) == '\u0000') {
+                            n--;
+                        }
+                    }
+                } catch (SocketTimeoutException r) {
+                    System.out.println("Didn't receive answer");
                 }
             }
         } catch (IOException e) {

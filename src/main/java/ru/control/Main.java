123d95a3df2d4fbed7274c259c3d07289d93d337
Андрей <dromgnh@gmail.com>
2016-06-21 22:18:38 +0300

    almost fixed detecting LAN IPs

--- src/main/java/ru/control/Main.java
+++ src/main/java/ru/control/Main.java
@@ -2,8 +2,10 @@ package ru.control;
 
 import java.io.*;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.nio.CharBuffer;
 import java.nio.charset.Charset;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -19,7 +21,7 @@ public class Main {
             numbers[i] = sc.nextInt();
         }*/
         int[] numbers = {192, 168, 1};  //default for my LAN
-        Set<InetAddress> addressSet = getLanIPs(numbers);
+        Set<InetAddress> addressSet = getLanIPs();
         System.out.println("IPs list: " + addressSet);
         //Thread sender = new ru.control.ThreadSender();
         //Thread receiver = new ru.control.ThreadReceiver();
@@ -31,37 +33,57 @@ public class Main {
         }*/
     }
 
-    //TODO: isReachable badly works, fix it
+    //TODO: fix arp, because it doesn't show ips connected after me
+    //TODO: with network interface detect LAN ips
+    //TODO: make method parsing string ip to int array
     /**
+     * detecting computers LAN IP and pushes it to set
      * reading results from arp -a
      * dividing it's lines by spaces
      * and checking if divided part is an IP which consist necessary first numbers
-     * if it is then checking if it is reachable
-     * if it is then pushing it into a set
+     * if it is then pinging it
+     * if it pings then pushing it into a set
      */
-    private static Set<InetAddress> getLanIPs(int[] numbers) {
+    private static Set<InetAddress> getLanIPs() {
+        int[] numbers = null;
         Set<InetAddress> addressesSet = new HashSet<>();
-        ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "arp -a");
-        builder.redirectErrorStream(true);
+        ProcessBuilder arpBuilder = new ProcessBuilder("arp", "-a");
+        ProcessBuilder pingBuilder;
+        Process proc;
+        arpBuilder.redirectErrorStream(true);
         try {
-            Process proc = builder.start();
+            Enumeration<NetworkInterface> nics = NetworkInterface.getNetworkInterfaces();
+            while (nics.hasMoreElements()) {
+                NetworkInterface nic = nics.nextElement();
+                Enumeration<InetAddress> addrs = nic.getInetAddresses();
+                while (addrs.hasMoreElements()) {
+                    InetAddress addr = addrs.nextElement();
+                    if (nic.getName().contains("wlan") && addr.getHostAddress().matches("[0-9]*\\.[0-9]*\\.[0-9]*\\.[0-9]*")) {
+                        System.out.println("my LAN IP is " + addr.getHostAddress());
+                        addressesSet.add(addr);
+                        //TODO: convert byte[] to int[]
+                        //numbers = addr.getAddress();
+                    }
+                }
+            }
+            proc = arpBuilder.start();
             BufferedReader br = new BufferedReader(new InputStreamReader(proc.getInputStream(), "cp866"));
-            InetAddress address;
             String line = br.readLine();
             while (line != null) {
                 String[] parts = line.split(" ");
                 for (String part : parts) {
                     if (validIP(part, numbers)) {
-                        address = InetAddress.getByName(part);
-                        if (address.isReachable(5000)) {
-                            addressesSet.add(address);
+                        pingBuilder = new ProcessBuilder("ping", "-n", "1", part);
+                        proc = pingBuilder.start();
+                        if (proc.waitFor() == 0) {
+                            addressesSet.add(InetAddress.getByName(part));
                             break;
                         }
                     }
                 }
                 line = br.readLine();
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.err.println(e.getMessage());
         }
         return addressesSet;
@@ -69,17 +91,17 @@ public class Main {
 
     private static boolean validIP(String ip, int[] numbers) {
         try {
-            if ( ip == null || ip.isEmpty() ) {
+            if (ip == null || ip.isEmpty()) {
                 return false;
             }
 
             String[] parts = ip.split( "\\." );
-            if ( parts.length != 4 ) {
+            if (parts.length != 4) {
                 return false;
             }
 
             for (int i = 0; i < 3; i++) {
-                if (Integer.parseInt(parts[i]) != numbers[i]) {
+                if (Byte.parseByte(parts[i]) != numbers[i]) {
                     return false;
                 }
             }
